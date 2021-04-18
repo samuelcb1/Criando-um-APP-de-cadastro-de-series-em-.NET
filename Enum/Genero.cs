36fcf57e5f01b6b85793f003d7f6cd921e4a1051
samuelcb1 <82475889+samuelcb1@users.noreply.github.com>
2021-04-18 19:53:12 -0300

    Add files via upload

---
+++ Enum/Genero.cs
@@ -0,0 +1,19 @@
+namespace DIO.Series
+{
+	public enum Genero
+	{
+		Acao = 1,
+		Aventura = 2,
+		Comedia = 3,
+		Filmes_De_Animacao = 4,
+		Anime = 5,
+		Policial = 6,
+		Filmes_De_Guerra = 7,
+		Fantasia = 8,
+		Ficcao_Cientifica = 9,
+		Musical = 10,
+		Romance = 11,
+		Suspense = 12,
+		Terror = 13
+	}
+}
